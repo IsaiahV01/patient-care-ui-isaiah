13985e7f3d462d59fde0f788ace9a915b14cb1f2
Isaiah Velarde <isaiah.velarde01@gmail.com>
2025-02-17 17:40:32 -0600

    fixed main.js to include the router, main default page is now properly displayed

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import router from './router'
 
 // Import Font Awesome core
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -17,5 +18,5 @@ const app = createApp(App);
 
 // Register FontAwesome globally
 app.component('font-awesome-icon', FontAwesomeIcon);
-
+app.use(router);
 app.mount('#app');
