2264b6b3e04f9a11cef047eb8c52b671b4d50883
Isaiah Velarde <isaiah.velarde01@gmail.com>
2025-02-22 13:40:25 -0600

    Changed all the styling on each page to be in a styling folder in assests

--- src/views/AppointmentsView.vue
+++ src/views/AppointmentsView.vue
@@ -7,41 +7,7 @@
       <hr class="divider" />
     </div>
   </template>
-<style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..700;1,400..700&family=Montserrat:wght@100&display=swap');
-
-body {
-  margin: 0;
-}
-.appointments-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background: white;
-  color: #806df1;
-  text-align: center;
-}
-
-.montserrat-uniquifier {
-  font-family: "Montserrat", serif;
-  font-optical-sizing: auto;
-  font-weight: 100;
-  font-style: normal;
-}
-
-.title {
-  font-family: 'Montserrat', serif;
-  font-size: 7rem;
-  font-weight: bold;
-  letter-spacing: 20px;
-}
-
-.divider {
-  width: 85%;
-  border: 1px solid #806df1;
-  margin: 1rem 0;
-}
+<style>
+@import '../assets/styling/appointments.css';
 </style>
   
